f53f897962646b7d32e194fed4bb55dd5cd38b0c
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-25 11:22:34 +0600

    Add files via upload
    rtue

---
+++ App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/Layout/Main';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+       path: '/',
+       element: <Main></Main>,
+       children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        }
+       ]
+     }
+])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
